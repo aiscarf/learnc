409de46de33392289cd54da149001d442b9d2236
aiweijin <aiweijin>
2020-09-22 10:23:32 +0800

    feat : 006_libuv libuv库

---
+++ Projects/006_libuv/class_libuv/src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// #pragma 只在vs编译器下有用
+// #pragma comment (lib, "ws2_32.lib") 
+// #pragma comment (lib, "Iphlpapi.lib")
+// #pragma comment (lib, "Psapi.lib")
+// #pragma comment (lib, "Userenv.lib")
+
+int main(int argc, char** argv) {
+
+    system("pause");
+	return 0;
+}
